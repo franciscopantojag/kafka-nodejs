1f9884b60454a4eb79b11efbb3559a13bca28b02
Francisco Pantoja <franciscopantojag98@gmail.com>
2022-01-16 23:43:11 -0500

    clean files

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-const add = (a: number, b: number) => a + b;
-
-console.log(add(4, 4));
-console.log(fs.readFileSync(path.join(__dirname, 'test.json'), { encoding: 'utf-8' }));
